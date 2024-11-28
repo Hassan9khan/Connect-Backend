17864fa1cfac4bbf5c3f3a59a284a81bf2ace065
Hassan Ahmed <hassanshakeel381@gmail.com>
2024-11-28 14:30:05 +0500

    files updated

--- src/App.jsx
+++ src/App.jsx
@@ -34,18 +34,29 @@ const App = () => {
     }
   };
 
+  const deleteUser = async (id) => {
+    try {
+      const response = await axios.delete(`http://localhost:3000/user/${id}`);
+      console.log(response.data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   return (
     <>
       <form onSubmit={addTodo}>
         <input type="text" placeholder="enter title" ref={title} />
         <button type="submit">Add User</button>
       </form>
+      {loading && <h1>Loading...</h1>}
+      {error && <h1>Error...</h1>}
       {data ? (
         data.map((item) => {
           return (
             <div key={item.id}>
               <h1>{item.title}</h1>
-              <button onClick={() => console.log("hello")}>Delete</button>
+              <button onClick={() => deleteUser(item.id)}>Delete</button>
             </div>
           );
         })
